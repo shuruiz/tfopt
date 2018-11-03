bdbe004af221871e4634591b2e73116e1dd165d8
Ethan <zhangzhbd@gmail.com>
2018-11-02 23:53:17 -0400

    construct network

--- network.py
+++ network.py
@@ -4,41 +4,78 @@ import random
 """
 class to create network
 """
+from genetic_algorithm import GeneticAlgorithmTrainner
+
 
 
 class Network():
 	"""create a fully-connected network"""
-	def __init__():
+	def __init__(num_input):
 		"""
 		input layer is not considered as a layer
 		"""
 		self._nlayer=0
 		self._nnodes=[]
+		self._num_input = num_input
+		self._weights={} #{layer: Weights}
+		self._bias={}
 
-
-
-	def add_layer(n_nodes,n_layer=1):
+	def add_layer(n_nodes):
 		"""
 		add layers to the network
 		n_nodes: [n_node1, n_nodes_2]
 		"""
+		n_layer = len(n_nodes)
+
 		self._nlayer +=n_layer
-		self._bias  = np.random.randn(self._nlayer) # initialize bias
-		
+		# self._bias  = np.random.randn(1, self._nlayer) # initialize bias
 		for ele in n_nodes:
 			self._nnodes.append(n_nodes)
+		random_initialize()
+
+	def delete_layer(index):
+		pass
 
 	def random_initialize(self):
 		for l in range(self._nlayer):
-			w = np.random.randn(self._nnodes[l],self._nnodes[l- 1]) 
-			self._weights.append(w)
+			self._bias[l]  = np.random.randn()
+			Weights = []
+			if l ==0: # first layer
+				for n in range(self._nnodes[l]):
+					w = np.random.randn(1,self._num_input)
+					Weights.append(w[0])
+				Weights = np.array(Weights)
+				self._weights[l] = Weights
+
+			else:
+				for n in range(self._nnodes[l]):
+					w = np.random.randn(1, self._nnodes[l-1])
+					Weights.append(w[0])
+				Weights = np.array(Weights)
+				self._weights[l] = Weights
+
+	def train(data, problem, algo='genetic'):
+		"""
+
+		"""
+		if algo == genetic:
+			self._weights, self._bias, fitness = GeneticAlgorithmTrainner()
+
+		elif algo =='RHC':
+			pass 
+		elif algo =='SIMann':
+			pass 
+
+
+
 
 	def getWeights():
 		return self._weights
 
-	def output(self):
+	def getBias():
+		return self._bias
 
-		return self._output
+	def loss
 
 
 
