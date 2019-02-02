3f9c554830d30cab169cd0e226d2060bf1e8cebe
Ethan <zhangzhbd@gmail.com>
2019-02-01 22:00:27 -0500

    build genetic optimizer

---
+++ genetic.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import random 
+from network import Network
+from sklearn.utils.extmath import softmax
+from statistics import mean
+
+
+"""
+genetic algorithm for neural network update
+"""
+
+def mutation():
+	pass
+def crossover():
+	pass
+
+def GeneticOptimizer():
+	pass
